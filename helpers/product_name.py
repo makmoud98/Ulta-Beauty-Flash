abebf97c8072df55759b755deacdd2c9857d2162
Michael Elnajami <makmoud98@gmail.com>
2019-10-27 10:16:32 +0000

    fixed formatting issue with get_most_popular endpoint

--- helpers/product_name.py
+++ helpers/product_name.py
@@ -3,22 +3,27 @@ import numpy as np
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import pickle
+
 sku_to_image = pd.read_csv('static/sku_to_img.csv')
+product_catalog = pd.read_csv('static/Product_Catalog.psv', delimiter='|')
 
 def get_product_img_url(sku_id):
-    sku_url = sku_to_image[sku_to_image['SKU_ID'] == int(sku_id)].to_dict()
-    url_id = list(sku_url['img_url'].keys())[0]
-    url = sku_url['img_url'][url_id]
-    print(url)
+    url = sku_to_image[sku_to_image['SKU_ID'] == int(sku_id)]['img_url'].values[0]
     x = cloudinary.uploader.upload(url,crop="limit",tags="samples",width=100,height=100)
     return x['secure_url']
 
-product_catalog = pd.read_csv('static/Product_Catalog.psv', delimiter='|')
-def get_product_by_id(sku_id):
+def get_product_tojson(sku_id):
     """Get Product Display Name using a SKU_ID"""
     sku_id = int(sku_id)
-    x = product_catalog[product_catalog['SKU_ID'] == sku_id].to_dict(orient='index')
-    
-    x[list(x.keys())[0]]['img_url'] = get_product_img_url(sku_id)
-    print(x)
-    return x
+    row = product_catalog[product_catalog['SKU_ID'] == sku_id]
+    row = row.transpose().to_dict()
+    key = list(row.keys())[0]
+    row[key]['img_url'] = get_product_img_url(sku_id)
+    return row[key]
+
+def get_product(sku_id):
+    """Get Product Display Name using a SKU_ID"""
+    sku_id = int(sku_id)
+    row = product_catalog[product_catalog['SKU_ID'] == sku_id]
+    return row
