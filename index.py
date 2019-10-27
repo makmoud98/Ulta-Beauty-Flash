c2a02c20a90dfa5289059caea05430888436f254
Michael Elnajami <makmoud98@gmail.com>
2019-10-27 05:16:53 +0000

    cleaned store details db

--- index.py
+++ index.py
@@ -13,17 +13,9 @@ import pickle
 def path(file_name, path_name='static/'):
     return path_name+file_name
 
-def convert_address(address):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    params = {'key': config.API_KEY, 'address': '+'.join(address.split(' '))}
-    r = requests.get(url, params)
-    loc = json.loads(r.text)['results'][0]['geometry']['location']
-    return {'type':'Point', 'coordinates': [loc['lat'], loc['lng']]}
-
-store_hours_df = pd.read_csv(path("Store_Hours.csv"))
-#store_hours_df.info()
+store_hours_df = pickle.loads(open(path('store_hours.pickle'), 'rb').read())
 active_devices_df = pd.read_csv(path("ulta_beauty__makeup_&_skincare-active_devices-20190724-20191021.csv"), encoding = "ISO-8859-1")
-store_details_df = pickle.loads(open(path('new_product_details.txt'), 'rb').read())
+store_details_df = pickle.loads(open(path('store_details.pickle'), 'rb').read())
 sku_metadata_df = pd.read_csv(path("Sku_MetaData.csv"), encoding = "ISO-8859-1")
 product_catalog_df = pd.read_csv(path("Product_Catalog.csv"), encoding = "ISO-8859-1", sep = "|")
 product_catalog_df['CATEGORY_NAME'] = product_catalog_df['CATEGORY_NAME'].astype('category')
@@ -38,7 +30,7 @@ def get_recommendation():
     return jsonify(items=get_product_recommendations(keyword))
 
 def calc_dist(point, row): 
-    return distance.vincenty(point, (row.coords.xy[0][0], row.coords.xy[1][0])).km
+    return distance.vincenty(point, (row['coordinates'][0], row['coordinates'][1])).km
 
 def closest_row_func(point, gpd2):
     gpd2['Dist'] = gpd2['geometry'].apply(lambda row: calc_dist(point, row))
@@ -46,18 +38,15 @@ def closest_row_func(point, gpd2):
     closest_row = gpd2[gpd2['Dist'] == min_distance]
     return closest_row
 
-#(long, lat)
-#p = closest_row_func((-58.66, -34.58), gdf)
-
-#@app.route('/api/get_nearest_store')
-def get_nearest_store(lat, lng):
-    
-    return None
+@app.route('/api/get_nearest_store')
+def get_nearest_store():
+    lat = float(request.args.get("lat", "0"))
+    lng = float(request.args.get("lng", "0"))
+    closest_row = closest_row_func((lat,lng), store_details_df).iloc[0,:]
+    return closest_row.to_json(orient='index')
 
 def get_product_recommendations(keyword):
 	return []
-
-#convert_address("1200 Journey")
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
