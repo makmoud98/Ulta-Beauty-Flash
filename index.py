b31aae43fcb0001aaed3bc2e0e3221df6301e186
Arushi Rai <arushirai1999@gmail.com>
2019-10-26 19:51:37 -0500

    Address in right format

--- index.py
+++ index.py
@@ -3,13 +3,20 @@ app = Flask(__name__)
 import config
 import pandas as pd
 import geopandas as gpd
-'''
-path = 'static/'
-store_hours_df = pd.read_csv(path+"Store_Hours.csv")
-store_hours_df.info()
 
-active_devices_df = pd.read_csv("ulta_beauty__makeup_&_skincare-active_devices-20190724-20191021.csv", encoding = "ISO-8859-1")
-'''
+def path(file_name, path_name='static/'):
+    return path_name+file_name
+
+store_hours_df = pd.read_csv(path("Store_Hours.csv"))
+#store_hours_df.info()
+active_devices_df = pd.read_csv(path("ulta_beauty__makeup_&_skincare-active_devices-20190724-20191021.csv"), encoding = "ISO-8859-1")
+store_details_df = pd.read_csv(path("Store_Details.csv"), encoding = "ISO-8859-1")
+sku_metadata_df = pd.read_csv(path("Sku_MetaData.csv"), encoding = "ISO-8859-1")
+product_catalog_df = pd.read_csv(path("Product_Catalog.csv"), encoding = "ISO-8859-1", sep = "|")
+product_catalog_df['CATEGORY_NAME'] = product_catalog_df['CATEGORY_NAME'].astype('category')
+
+store_details_df['address']=store_details_df['ADDRESS_1']+', '+store_details_df['ADDRESS_2']+' ' + store_details_df['CITY']+' '+store_details_df['STATE']+' ' + str(store_details_df['ZIPCODE'])
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
