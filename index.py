8674143397155e059d82ef7d2d8802a57e97a4a1
Arushi Rai <arushirai1999@gmail.com>
2019-10-26 19:14:58 -0500

    Fix error

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+import config
+
 
 @app.route('/')
 def hello_world():
@@ -10,8 +12,20 @@ def get_recommendation():
     keyword = request.args.get("keyword", "lipstick")
     return jsonify(items=get_product_recommendations(keyword))
 
+@app.route('/api/get_nearest_store')
+def get_nearest_store():
+    return None
+
+def convert_address(address):
+    #address.split
+    #https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
+    #+Mountain+View,+CA&key=YOUR_API_KEY
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address +'&key='+ config.API_KEY
+    return None
+
 def get_product_recommendations(keyword):
 	return []
+
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
