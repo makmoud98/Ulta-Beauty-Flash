b0b58e8a5e08b0342ab16170e9b1aea6ca22c493
Arushi Rai <arushirai1999@gmail.com>
2019-10-26 19:35:22 -0500

    Split address

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 import config
+import pandas as pd
+import geopandas as gpd
+'''
+path = 'static/'
+store_hours_df = pd.read_csv(path+"Store_Hours.csv")
+store_hours_df.info()
 
-
+active_devices_df = pd.read_csv("ulta_beauty__makeup_&_skincare-active_devices-20190724-20191021.csv", encoding = "ISO-8859-1")
+'''
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -17,15 +24,17 @@ def get_nearest_store():
     return None
 
 def convert_address(address):
-    #address.split
+    address = '+'.join(address.split(' '))
     #https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
     #+Mountain+View,+CA&key=YOUR_API_KEY
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address +'&key='+ config.API_KEY
+    print(url)
     return None
 
 def get_product_recommendations(keyword):
 	return []
 
+#convert_address("1200 Journey")
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5000)
