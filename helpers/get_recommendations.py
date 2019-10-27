f1ffd6e6e57ee15a6901d266eba922a1094c0863
ericktorres <ericktorresdeveloper@gmail.com>
2019-10-27 00:51:08 -0500

    added helper functions

---
+++ helpers/get_recommendations.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def get_most_popular():
+    """Function that returns the most popular items"""
+    utility = pd.read_csv('utility_matrix.csv')
+    total_purchases = utility.sum(axis=0)
+    sorted_purchases = sorted(range(len(total_purchases)), key=lambda k: total_purchases[k], reverse=True)[1:]
+    return utility.columns[sorted_purchases[:5]].tolist()
---
+++ helpers/product_name.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def get_product_name(sku_id):
+    """Get Product Display Name using a SKU_ID"""
+    sku_id = int(sku_id)
+    product_catalog = pd.read_csv('Product_Catalog.psv', delimiter='|')
+    return product_catalog[product_catalog['SKU_ID'] == sku_id]['DISPLAY_NAME'].values[0]
