04dbc49002031d64f5db496e3d71f78428855f7a
ericktorres <ericktorresdeveloper@gmail.com>
2019-10-27 05:27:05 -0500

    Added recommendation files and methods

--- helpers/get_recommendations.py
+++ helpers/get_recommendations.py
@@ -4,16 +4,45 @@ from helpers.product_name import get_product_tojson
 
 utility = pd.read_csv('static/utility_matrix.csv')
 
+
 def get_most_popular():
     """Function that returns the most popular items"""
     total_purchases = utility.sum(axis=0)[1:]
     indices = list(map(int, total_purchases.index.tolist()))
-    total_purchases = [(indices[i], x) for i, x in enumerate(total_purchases)] 
-    total_purchases = sorted(total_purchases, key = lambda x: x[-1], reverse = True) 
+    total_purchases = [(indices[i], x) for i, x in enumerate(total_purchases)]
+    total_purchases = sorted(
+        total_purchases, key=lambda x: x[-1], reverse=True)
     top = total_purchases[:5]
 
     items = [get_product_tojson(x[0]) for x in top]
     return items
 
-def get_product_recommendations(keyword):
-	return []
+
+user_recs = open('../static/user_recs.txt')
+user_hist = open('../static/user_history.txt')
+category_array = open('../static/categories.txt')
+
+lst_recs = eval(user_recs.read())
+lst_recs = lst_recs[0]
+hist = eval(user_hist.read())
+
+user_recs.close()
+user_hist.close()
+
+def find_top_n(lst, n):
+    lst = [x[-1] for x in lst] # remove tups 
+    choices = np.random.choice(lst, n)
+    # remove dups 
+    choices = set(choices) 
+    choices = list(choices)
+    return choices
+
+def find_keyword(keyword):
+    index = category_array.index(keyword)
+    return lst_recs[index]
+
+def get_product_recommendations(keyword, n=5):
+    keyword_lst = find_keyword(keyword)
+    sku_lst = find_top_n(keyword_lst, n)
+    items = [get_product_tojson(x) for x in sku_lst]
+    return items
